f3bf097689edf48bd4a773cf82b2873a83003855
Sowmya241220 <gundetisowmya05@gmail.com>
2024-12-01 13:33:39 +0530

    updated demo file

---
+++ citibank/demo.java
@@ -0,0 +1,15 @@
+int m=1 , n=20 , count=0
+for(int i=1;i<=20;i++){
+int num=i;
+while (num%10==3){
+int num=i;
+while(num>0){
+if(num%10==3){
+count++;
+}
+num=num/10;
+}
+}
+system.out.println(count);
+}
+}
